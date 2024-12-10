42c05821ce86e2d083f45dbf6e2ad6206ab67c25
mohammedateeef <atefm.me556@gmail.com>
2024-12-10 11:50:12 +0200

    first commit

--- src/app.ts
+++ src/app.ts
@@ -13,3 +13,28 @@ app.use('/', (req: Request, res: Response, next: NextFunction) => {
 export default app;
 
 // npm i express; npm i -D ts-node @types/express
+// npm install ts-node-dev dotenv --save-dev
+
+// package.json
+// "scripts": {
+//   "dev": "ts-node-dev --respawn --transpile-only --ignore-watch node_modules --watch src --env-file .env src/index.ts",
+//   "build": "tsc",
+//   "start": "node dist/index.js"
+// }
+
+// app.ts
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import express from "express";
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello, world!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
