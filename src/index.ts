bfbf1c8aa581ceb19198383b5a6ceb1380a1664c
mohammedateeef <atefm.me556@gmail.com>
2025-01-12 15:04:42 +0200

    chnages

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,56 @@ app.listen(config.port, async () => {
   await connectDB();
   console.log(`Server is running on http://localhost:${config.port}`);
 });
+
+// const fs = require('fs');
+// const path = require('path');
+// const { exec } = require("child_process");
+
+// // Playlist URL
+// const playlistUrl = 'https://www.youtube.com/playlist?list=PLEe8jzHjtRiRQWZugdORFlqCGh18fwnSz';
+
+// // Output directory for videos
+// const outputDir = path.join(__dirname, 'downloads');
+
+// // Ensure the output directory exists
+// if (!fs.existsSync(outputDir)) {
+//   fs.mkdirSync(outputDir);
+// }
+
+// const execPromise = (command) => {
+//   return new Promise((resolve, reject) => {
+//     exec(command, { shell: true }, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(
+//           `Command failed with exit code ${error.code}: ${
+//             stderr || error.message
+//           }`
+//         );
+//         return reject(new Error(stderr || error.message));
+//       }
+//       resolve(stdout);
+//     });
+//   });
+// };
+
+// // Download playlist
+// async function downloadPlaylist(playlistUrl) {
+//   try {
+//     console.log('Starting playlist download...');
+
+//     // Construct the yt-dlp command
+//     const command = `yt-dlp -o "${path.join(outputDir, '%(title)s.%(ext)s')}" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" "${playlistUrl}"`;
+
+//     console.log(`Executing command: ${command}`);
+
+//     // Execute the command
+//     await execPromise(command);
+
+//     console.log('All videos downloaded!');
+//   } catch (error) {
+//     console.error('An error occurred:', error);
+//   }
+// }
+
+// // Start downloading
+// downloadPlaylist(playlistUrl);
